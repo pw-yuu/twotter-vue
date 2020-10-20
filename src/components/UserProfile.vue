37e82373f67fdcdcb95d6195058f37c28b2d8d36
Philippe huynh <pw.yuupi@gmail.com>
2020-10-20 17:57:21 +0900

    added styling with sass

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -10,8 +10,8 @@
                 <strong>Followers: </strong>{{ followers }}
             </div>
 
-            <form class="user-profile__create-twoot" @submit.prevent="createNewTwoot">
-                <label for="newTwoot"><strong>New Twoot</strong></label>
+            <form class="user-profile__create-twoot" @submit.prevent="createNewTwoot" :class="{ '--exceeded': newTwootCharacterCount > 180 }">
+                <label for="newTwoot"><strong>New Twoot</strong> ({{ newTwootCharacterCount }}/180)</label>
                 <textarea id="newTwoot" rows="4" v-model="newTwootContent"/>
 
                 <div class="user-profile__create-twoot-type">
@@ -90,6 +90,9 @@ export default {
     computed: {
         fullName() {
             return `${this.user.firstName} ${this.user.lastName}`;
+        },
+        newTwootCharacterCount() {
+            return this.newTwootContent.length;
         }
     },
     methods: {
@@ -115,45 +118,57 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
 .user-profile {
     display: grid;
     grid-template-columns: 1fr 3fr;
+    grid-gap: 50px;
     padding: 50px 5%;
-}
 
-.user-profile__user-panel {
-    display: flex;
-    flex-direction: column;
-    margin-right: 50px;
-    padding: 20px;
-    background-color: white;
-    border-radius: 5px;
-    border: 1px solid #DFE3E8;
-}
+    .user-profile__user-panel {
+        display: flex;
+        flex-direction: column;
+        margin-right: 50px;
+        padding: 20px;
+        background-color: white;
+        border-radius: 5px;
+        border: 1px solid #DFE3E8;
 
-.user-profile__admin-badge {
-    background: purple;
-    color: white;
-    border-radius: 5px;
-    margin-right: auto;
-    padding: 0 10px;
-    font-weight: bold;
-}
+        h1 {
+            margin: 0;
+        }
+        
+        .user-profile__admin-badge {
+            background: purple;
+            color: white;
+            border-radius: 5px;
+            margin-right: auto;
+            padding: 0 10px;
+            font-weight: bold;
+        }
 
-h1 {
-    margin: 0;
+        .user-profile__create-twoot {
+            padding-top: 20px;
+            display: flex;
+            flex-direction: column;
+
+            &.--exceeded {
+                color: red;
+                border-color: red;
+
+                button {
+                    background-color: red;
+                    border: none;
+                    color: white;
+                }
+            }
+        }
+    }
 }
 
 .user-profile__twoots-wrapper {
     display: grid;
     grid-gap: 10px;
-}
-
-.user-profile__create-twoot {
-    padding-top: 20px;
-    display: flex;
-    flex-direction: column;
 }
 
 </style>
