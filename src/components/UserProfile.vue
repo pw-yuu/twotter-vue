99d4ef1dbbb48eb49ffaf93419ecd8d6b2f7011b
Philippe huynh <pw.yuupi@gmail.com>
2020-10-20 16:52:47 +0900

    VUe review 1

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,147 @@
+<template>
+    <div class="user-profile">
+
+        <div class="user-profile__user-panel">
+            <h1 class="user-profile__username">@{{user.username}}</h1>
+            <div class="user-profile__admin-badge" v-if="user.isAdmin">
+                admin
+            </div>
+            <div class="user-profile__follower-count">
+                <strong>Followers: </strong>{{ followers }}
+            </div>
+
+            <form class="user-profile__create-twoot">
+
+                <label for="newTwoot"><strong>New Twoot</strong></label>
+                <textarea id="newTwoot" rows="4"/>
+
+                <div class="user-profile__create-twoot-type">
+                    <label for="newTwootType"><strong>Type: </strong></label>
+
+                    <select id="newTwootType">
+                        <option :value="option.value" v-for="(option, index) in twootTypes" :key="index">
+                            {{ option.name }}
+                        </option>
+                    </select>
+                </div>
+
+            </form>
+        </div>
+
+
+        <div class="user-profile__twoots-wrapper">
+            <TwootItem
+                v-for="twoot in user.twoots"
+                :key="twoot.id"
+                :username="user.username"
+                :twoot="twoot"
+                @favourite="toggleFavourite"
+            />
+        </div>
+
+    </div>
+</template>
+
+<script>
+import TwootItem from "./TwootItem";
+
+export default {
+    name: "UserProfile",
+    components: {
+        TwootItem
+    },
+    data() {
+        return {
+            twootTypes: [
+                { value: 'draft', name: 'Draft' },
+                { value: 'instant', name: 'Instant'}
+            ],
+            followers: 0,
+            user: {
+                id: 1,
+                username: '_pwy',
+                firstName: 'Philipe',
+                lastName: 'Huynh',
+                email: 'pwy@pwy.co',
+                isAdmin: true,
+                twoots: [
+                    {
+                        id: 1,
+                        content: 'Twotter is amazing!'
+                    },
+                    {
+                        id: 2,
+                        content: "Don't forget to eat your veggies"
+                    }
+                ]
+            }
+        }
+    },
+    watch: {
+        followers(newFollowerCount, oldFollowerCount) {
+            if (oldFollowerCount < newFollowerCount) {
+                console.log(`${this.user.username} has gained a follower!`);
+            }
+        }
+    },
+    computed: {
+        fullName() {
+            return `${this.user.firstName} ${this.user.lastName}`;
+        }
+    },
+    methods: {
+        followUser() {
+            this.followers++;
+        },
+        toggleFavourite(id) {
+            console.log(`Favourited Tweet ${id}`)
+        }
+    },
+    mounted() {
+        this.followUser();
+    }
+};
+</script>
+
+<style>
+.user-profile {
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    padding: 50px 5%;
+}
+
+.user-profile__user-panel {
+    display: flex;
+    flex-direction: column;
+    margin-right: 50px;
+    padding: 20px;
+    background-color: white;
+    border-radius: 5px;
+    border: 1px solid #DFE3E8;
+}
+
+.user-profile__admin-badge {
+    background: purple;
+    color: white;
+    border-radius: 5px;
+    margin-right: auto;
+    padding: 0 10px;
+    font-weight: bold;
+}
+
+h1 {
+    margin: 0;
+}
+
+.user-profile__twoots-wrapper {
+    display: grid;
+    grid-gap: 10px;
+}
+
+.user-profile__create-twoot {
+    padding-top: 20px;
+    display: flex;
+    flex-direction: column;
+}
+
+</style>
